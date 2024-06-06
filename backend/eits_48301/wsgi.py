5269bc767bf182d8850811db287358fbb7a631f3
Crowdbotics <team@crowdbotics.com>
2024-06-06 17:09:18 +0000

    Initial commit

---
+++ backend/eits_48301/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for eits_48301 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "eits_48301.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "eits-48301",
+  url: "https://eits-48301.botics.co",
+  api: "https://eits-48301.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
